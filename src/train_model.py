a17859eb01887c83c2f466fd9dc08046b5a2a119
anna802698 <anna802698@gmail.com>
2026-04-10 13:48:10 +0530

    initial commit

---
+++ src/train_model.py
@@ -0,0 +1,23 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from preprocess import preprocess_data
+
+def train_model():
+
+    X_train, X_test, y_train, y_test = preprocess_data()
+
+    # Train model
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    # Prediction
+    predictions = model.predict(X_test)
+
+    # Accuracy
+    accuracy = accuracy_score(y_test, predictions)
+
+    print("Model Training Completed")
+    print("Model Accuracy:", accuracy)
+
+if __name__ == "__main__":
+    train_model()
